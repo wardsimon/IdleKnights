e4a29b6c1ade574f090d08334557f9abbcf6a69d
Simon Ward <2798086+wardsimon@users.noreply.github.com>
2023-02-16 14:02:38 +0100

    Clean up workflow file

--- scripts/generate_html.py
+++ scripts/generate_html.py
@@ -5,22 +5,22 @@ import sys
 import requests
 
 org = "wardsimon"
-repo = "IdleKnights"
-if len(sys.argv) > 1:
-    repo = sys.argv[1]
+repo = sys.argv[1]
+token = sys.argv[2]
 
-releases = requests.get(f"https://api.github.com/repos/{org}/{repo}/releases").json()
+headers = {'Authorization': 'token ' + token}
+releases = requests.get(f"https://api.github.com/repos/{org}/{repo}/releases", headers=headers).json()
 
 header = f"<!DOCTYPE html>\n<html>\n<head>\n<title>Links for {repo}</title>\n</head>\n<body>\n<h1>Links for {repo}</h1>"
 body = ""
 for release in releases:
     asset_url = release["assets_url"]
-    assets = requests.get(asset_url).json()
+    assets = requests.get(asset_url, headers=headers).json()
     for asset in assets:
         if asset["name"].endswith(".whl"):
             name = asset["name"][:-4]
             url = asset["browser_download_url"]
-            body += f'<a href="{url}" data-requires-python="&gt=3.7,&lt4.0">{name}</a><br/>\n'
+            body += f'<a href="{url}" data-requires-python="&gt=3.8,&lt4.0">{name}</a><br/>\n'
 footer = "</body>\n</html>"
 
 content = "\n".join([header, body, footer])
